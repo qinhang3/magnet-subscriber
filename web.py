7160df85c161d1cfd206fd420139368ba46d7494
qinhang.qh <qinhang.qh@alibaba-inc.com>
2018-02-27 23:54:22 +0800

    threaded=True

--- web.py
+++ web.py
@@ -55,4 +55,4 @@ api.add_resource(Task, '/ajax/task', '/ajax/task/<name>', '/ajax/task/<offset>/<
 
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0')
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, threaded=True)
