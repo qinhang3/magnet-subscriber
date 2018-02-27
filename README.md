49fb45478b919804de1844c27a2653c104c17456
qinhang.qh <qinhang.qh@alibaba-inc.com>
2018-02-27 23:18:11 +0800

    disabled debug add run shell

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 订阅根据关键字订阅BT磁力链
 前端：vue+element ui+axio
 后端：python+Flask RESTful+sqlite
+
+#Run
+```
+sudo pip install HTMLParser
+sudo pip install flask
+sudo pip install flask-restful
+sudo python web.py
+```
--- web.py
+++ web.py
@@ -54,4 +54,5 @@ class Task(restful.Resource):
 api.add_resource(Task, '/ajax/task', '/ajax/task/<name>', '/ajax/task/<offset>/<count>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
